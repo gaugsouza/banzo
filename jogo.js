0e53001f1c12922e4fb2ce07fa13f265d90b153a
gaugsouza <guilhermeguis@outlook.com>
2019-12-15 05:58:27 -0300

    Game core

--- jogo.js
+++ jogo.js
@@ -9,6 +9,7 @@ let botao1 = document.getElementById('botao1')
 function mudarImagem() {
     h1.innerHTML = "O QUE É HTML?"
     p.innerHTML = "Vamos começar com HTML, a linguagem de marcação responsável pela estrutura das páginas web na internet. A linguagem HTML diz para o browser como ler os conteúdos do site, como imagem, links e textos."
+    imagem.classList.replace("imagem", "posImagem");
     imagem.src = "./images/template.png"
     segundoBotao.style.display = 'block'
     botao1.style.display = 'none'
